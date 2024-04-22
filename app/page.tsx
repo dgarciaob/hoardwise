658549bf5210385b9573d753657b83f5c4f861c9
Diego <diegogaroban@hotmail.com>
2024-04-22 13:45:56 -0500

    add: dashboard png for landing

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
         <div className="mt-16 flow-root sm:mt-12 animate-circle-grow">
           <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             <Image
-              src="/dashboard.jpg"
+              src="/hoarddashboard.png"
               width={2560}
               height={1664}
               alt="Product preview"
